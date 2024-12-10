527491f4589b5941860e36f6f1773d5e6ddc6d76
Srujan S P <srujansjapthi@gmail.com>
2024-12-10 10:35:19 +0530

    Frontend Setup with TS, ShadCN, Tailwind CSS

---
+++ frontend/src/AppRoutes.tsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import AppLayout from "./layouts/AppLayout";
+import Home from "./pages/home/Home";
+import NotFound from "./pages/not-found/NotFound";
+
+const AppRoutes = () => {
+	return (
+		<Routes>
+			<Route path="/" element={<AppLayout />}>
+				<Route index element={<Home />} />
+				{/* Other pages which needs layout */}
+			</Route>
+
+			{/* Pages which doesn't need layout */}
+
+			<Route path="*" element={<NotFound />} />
+		</Routes>
+	);
+};
+
+export default AppRoutes;
---
+++ frontend/src/layouts/components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+	return (
+		<div className="flex h-16 items-center justify-between px-4">
+			<div className="text-2xl font-bold">Navbar</div>
+		</div>
+	);
+};
+
+export default Navbar;
