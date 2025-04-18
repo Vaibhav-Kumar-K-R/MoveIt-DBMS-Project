de09a4bb7466a774e758418601c43fa97b002cca
Vaibhav-Kumar-K-R <vaibhavkumar.k.r.cnr@gmail.com>
2025-04-18 14:07:53 +0530

    Deployment config changes

--- admin/src/layouts/components/ProfileButton.tsx
+++ admin/src/layouts/components/ProfileButton.tsx
@@ -8,15 +8,15 @@ function ProfileButton() {
       <SidebarMenuButton className="h-fit">
         <div className="w-full flex justify-start gap-2  ">
           <img
-            src={user.profile_img_url}
+            src={user?.profile_img_url}
             className="rounded-md  "
             width={75}
             height={75}
             alt="admin_profile_image"
           />
           <div className="flex justify-center items-center flex-col">
-            <p className="font-bold">{user.name}</p>
-            <p className="font-semibold">{user.email}</p>
+            <p className="font-bold">{user?.name}</p>
+            <p className="font-semibold">{user?.email}</p>
           </div>
         </div>
       </SidebarMenuButton>
--- backend/src/index.js
+++ backend/src/index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin:"*",
+    origin:"https://moveitadmin.netlify.app",
     credentials: true,
   }),
 );
